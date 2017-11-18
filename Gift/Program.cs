263ef05d6c0cd9f6c44fe5c98b98abc10ce9360d
Alexey Shpavda <alexeyshpavda@gmail.com>
2017-11-18 23:45:13 +0300

    Сreated variables

--- Gift/Program.cs
+++ Gift/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gift.Models;
 
 namespace Gift
 {
@@ -10,7 +11,12 @@ namespace Gift
     {
         static void Main()
         {
+            string l = "====================================================================================================================";
 
+            Gift.Models.Gift gift = new Gift.Models.Gift();
+            gift.ReadingFile("input.txt");
+
+            Console.ReadKey();
         }
     }
 }
