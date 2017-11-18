65d877a5534fa4cd0fd49a7f86410dfae8c55fd0
Alexey Shpavda <alexeyshpavda@gmail.com>
2017-11-18 23:39:39 +0300

    Add Enums

---
+++ Gift/Enums/Taste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gift.Enums
+{
+    public enum Taste
+    {
+        Vanilla,
+        Almond,
+        Limon
+    }
+}
