4914947a40447b3cf11b6ba477c29be5baececc8
Alexey Shpavda <alexeyshpavda@gmail.com>
2017-11-18 19:27:44 +0300

    Add project

--- Gift/Properties/AssemblyInfo.cs
+++ Gift/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Gifts")]
+[assembly: AssemblyTitle("Gift")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gifts")]
+[assembly: AssemblyProduct("Gift")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("c5ee239c-1b60-44bf-990f-93f15ea0b36e")]
+[assembly: Guid("5ea5ebdc-a383-4290-8e6c-a416b7468eda")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
